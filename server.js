4aadaddbb349ce9de65c414564bd0ae61b925dbc
khunlinnaing <khunlinnaingoo90@gmail.com>
2024-03-29 17:16:54 +0630

    testing

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) =>{
+    res.json({
+        status: 200,
+        message: 'Hello world'
+    })
+});
+app.listen(8000,() =>{
+    console.log('server is running on port 8000');
+})
